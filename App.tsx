9b5fb0a340d6a21090ae9b6f4ec05593303936c5
MAYERGANG <nickitamaier@yandex.ru>
2026-02-10 01:09:48 +0300

    Make background ultra cinematic

--- App.tsx
+++ App.tsx
@@ -326,9 +326,12 @@ const App: React.FC = () => {
 
   return (
     <div className="app-shell min-h-screen flex flex-col px-4 pt-6 pb-10 md:px-8 md:pt-10 max-w-6xl mx-auto">
+      <div className="bg-halo" />
+      <div className="bg-fog" />
       <div className="bg-orb bg-orb-a" />
       <div className="bg-orb bg-orb-b" />
       <div className="bg-orb bg-orb-c" />
+      <div className="bg-stars" />
       <div className="bg-mesh" />
       <div className="bg-noise" />
       <div className="bg-grid" />
